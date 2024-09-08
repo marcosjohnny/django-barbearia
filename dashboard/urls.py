9645d398d46fb9d4ececb502c02341d7cb9d9232
Marcos Rodrigues <38053209+marcosjohnny@users.noreply.github.com>
2024-09-08 22:23:32 +0000

    :sparkles: `feat`: forms add product

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, AddProductView
 
 urlpatterns = [
     path("", HomeView, name="homepage"),
+    path("adicionar/", AddProductView, name="add"),
+
 ]
